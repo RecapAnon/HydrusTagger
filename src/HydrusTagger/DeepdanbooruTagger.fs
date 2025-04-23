314386ebb6528ac9c28968254b77d73893b8e6dd
RecapAnon <129630050+RecapAnon@users.noreply.github.com>
2025-04-23 18:35:34 -0400

    Refactor out deepdanbooru tagging.

---
+++ src/HydrusTagger/DeepdanbooruTagger.fs
@@ -0,0 +1,76 @@
+module DeepdanbooruTagger
+
+open Microsoft.ML.OnnxRuntime
+open Microsoft.ML.OnnxRuntime.Tensors
+open SixLabors.ImageSharp
+open SixLabors.ImageSharp.PixelFormats
+open SixLabors.ImageSharp.Processing
+open System.IO
+open System.Linq
+open System.Text.Json
+
+type Tagger(session: InferenceSession, tags: string[]) =
+    let imageToOnnx (imageBytes: byte array) (size: int) =
+        use stream = new MemoryStream(imageBytes)
+        use image = Image.Load<Rgb24>(stream)
+
+        let h, w = image.Height, image.Width
+
+        let h', w' =
+            if h > w then
+                (size, int (float size * float w / float h))
+            else
+                (int (float size * float h / float w), size)
+
+        image.Mutate(fun c ->
+            c.Resize(w', h', KnownResamplers.Lanczos3) |> ignore
+            c.Pad(size, size, Color.White) |> ignore)
+
+        let width = image.Width
+        let height = image.Height
+
+        for y in 0 .. (height - h') / 2 do
+            for x in 0 .. width - 1 do
+                image[x, y] <- image[x, (height - h') / 2 + 1]
+
+        for y in height - (height - h') / 2 .. height - 1 do
+            for x in 0 .. width - 1 do
+                image[x, y] <- image[x, height - (height - h') / 2 - 1]
+
+        for y in 0 .. height - 1 do
+            for x in 0 .. (width - w') / 2 do
+                image[x, y] <- image[(width - w') / 2 + 1, y]
+
+        for y in 0 .. height - 1 do
+            for x in width - (width - w') / 2 .. width - 1 do
+                image[x, y] <- image[width - (width - w') / 2 - 1, y]
+
+        let tensor = new DenseTensor<float32>([| 1; size; size; 3 |])
+
+        for y = 0 to size - 1 do
+            for x = 0 to size - 1 do
+                let pixel = image[x, y]
+                tensor[0, y, x, 0] <- float32 pixel.R / 255.0f
+                tensor[0, y, x, 1] <- float32 pixel.G / 255.0f
+                tensor[0, y, x, 2] <- float32 pixel.B / 255.0f
+
+        tensor
+
+    member _.Identify(imageBytes: byte array) =
+        let tensor = imageToOnnx imageBytes 512
+        let inputs = [ NamedOnnxValue.CreateFromTensor("inputs", tensor) ]
+        let results = session.Run(inputs)
+        let outputTensor = results[0].AsTensor<float32>()
+        let probs = outputTensor.ToArray()
+
+        Array.zip tags probs
+        |> Array.filter (fun (_, score) -> score >= 0.5f)
+        |> Array.map fst
+        |> Array.toList
+
+    static member Create(modelPath: string) =
+        let session = new InferenceSession(modelPath)
+        let tags =
+            session.ModelMetadata.CustomMetadataMap["tags"]
+            |> JsonSerializer.Deserialize<string[]>
+        Tagger(session, tags)
--- src/HydrusTagger/Program.fs
+++ src/HydrusTagger/Program.fs
@@ -1,16 +1,10 @@
 ﻿open HydrusApi
+open DeepdanbooruTagger
 open Microsoft.Extensions.Configuration
-open Microsoft.ML.OnnxRuntime
-open Microsoft.ML.OnnxRuntime.Tensors
-open SixLabors.ImageSharp
-open SixLabors.ImageSharp.PixelFormats
-open SixLabors.ImageSharp.Processing
 open System
 open System.CommandLine
 open System.IO
-open System.Linq
 open System.Reflection
-open System.Text.Json
 open System.Threading.Tasks
 
 type Command = CommandLine.Command
@@ -37,73 +31,11 @@ let setGlobalHandler handler argument (command: RootCommand) =
 
 let invoke (argv: string array) (rc: RootCommand) = rc.Invoke argv
 
-let imageToOnnx (imageBytes: byte array) (size: int) =
-    use stream = new MemoryStream(imageBytes)
-    use image = Image.Load<Rgb24>(stream)
-
-    let h, w = image.Height, image.Width
-
-    let h', w' =
-        if h > w then
-            (size, int (float size * float w / float h))
-        else
-            (int (float size * float h / float w), size)
-
-    image.Mutate(fun c ->
-        c.Resize(w', h', KnownResamplers.Lanczos3) |> ignore
-        c.Pad(size, size, Color.White) |> ignore)
-
-    let width = image.Width
-    let height = image.Height
-
-    for y in 0 .. (height - h') / 2 do
-        for x in 0 .. width - 1 do
-            image[x, y] <- image[x, (height - h') / 2 + 1]
-
-    for y in height - (height - h') / 2 .. height - 1 do
-        for x in 0 .. width - 1 do
-            image[x, y] <- image[x, height - (height - h') / 2 - 1]
-
-    for y in 0 .. height - 1 do
-        for x in 0 .. (width - w') / 2 do
-            image[x, y] <- image[(width - w') / 2 + 1, y]
-
-    for y in 0 .. height - 1 do
-        for x in width - (width - w') / 2 .. width - 1 do
-            image[x, y] <- image[width - (width - w') / 2 - 1, y]
-
-    let tensor = new DenseTensor<float32>([| 1; size; size; 3 |])
-
-    for y = 0 to size - 1 do
-        for x = 0 to size - 1 do
-            let pixel = image[x, y]
-            tensor[0, y, x, 0] <- float32 pixel.R / 255.0f
-            tensor[0, y, x, 1] <- float32 pixel.G / 255.0f
-            tensor[0, y, x, 2] <- float32 pixel.B / 255.0f
-
-    tensor
-
-let identify (session: InferenceSession) (imageBytes: byte array) =
-    let tensor = imageToOnnx imageBytes 512
-    let inputs = [ NamedOnnxValue.CreateFromTensor("inputs", tensor) ]
-    let results = session.Run(inputs)
-    let outputTensor = results[0].AsTensor<float32>()
-    let probs = outputTensor.ToArray()
-
-    let tags =
-        session.ModelMetadata.CustomMetadataMap["tags"]
-        |> JsonSerializer.Deserialize<string[]>
-
-    Array.zip tags probs
-    |> Array.filter (fun (_, score) -> score >= 0.5f)
-    |> Array.map fst
-    |> Array.toList
-
 let handler appSettings (tags: string[]) : Task =
     let hydrusClient =
         HydrusApiClient(appSettings.BaseUrl, appSettings.HydrusClientAPIAccessKey)
 
-    let session = new InferenceSession(appSettings.ResnetModelPath)
+    let tagger = Tagger.Create(appSettings.ResnetModelPath)
 
     task {
         let! fileIdsResponse = hydrusClient.GetFiles(List.ofArray tags)
@@ -116,7 +48,7 @@ let handler appSettings (tags: string[]) : Task =
                 match filePathResponse with
                 | Some pathResponse ->
                     printfn "Path for file_id %i: %s" fileId pathResponse.path
-                    let newTags = identify session (File.ReadAllBytes pathResponse.path)
+                    let newTags = tagger.Identify(File.ReadAllBytes pathResponse.path)
                     printfn "Tags: %A" newTags
 
                     let request =
@@ -129,7 +61,7 @@ let handler appSettings (tags: string[]) : Task =
 
                     if not (Array.isEmpty fileBytes) then
                         printfn "File downloaded for file_id %i. Size: %i bytes" fileId fileBytes.Length
-                        let newTags = identify session fileBytes
+                        let newTags = tagger.Identify fileBytes
                         printfn "Tags: %A" newTags
 
                         let request =
