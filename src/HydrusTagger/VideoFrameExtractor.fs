50ab1fdb15b774a698c9d3751ae7506b1b8a240f
RecapAnon <129630050+RecapAnon@users.noreply.github.com>
2025-07-05 08:51:23 -0400

    Fix VideoFrameExtractor namespace.

--- src/HydrusTagger/VideoFrameExtractor.fs
+++ src/HydrusTagger/VideoFrameExtractor.fs
@@ -1,58 +1,59 @@
-module VideoFrameExtractor
+namespace HydrusTagger
 
 open System
 open System.Diagnostics
 open System.IO
 
-let getVideoDuration videoFilePath =
-    let parameters =
-        $"-v error -show_entries format=duration -of \"csv=p=0\" \"{videoFilePath}\""
+module VideoFrameExtractor =
+    let getVideoDuration videoFilePath =
+        let parameters =
+            $"-v error -show_entries format=duration -of \"csv=p=0\" \"{videoFilePath}\""
 
-    use proc = new Process()
-    proc.StartInfo.FileName <- "ffprobe"
-    proc.StartInfo.Arguments <- parameters
-    proc.StartInfo.RedirectStandardOutput <- true
-    proc.StartInfo.UseShellExecute <- false
-    proc.StartInfo.CreateNoWindow <- true
+        use proc = new Process()
+        proc.StartInfo.FileName <- "ffprobe"
+        proc.StartInfo.Arguments <- parameters
+        proc.StartInfo.RedirectStandardOutput <- true
+        proc.StartInfo.UseShellExecute <- false
+        proc.StartInfo.CreateNoWindow <- true
 
-    proc.Start() |> ignore
-    let output = proc.StandardOutput.ReadToEnd()
-    proc.WaitForExit()
+        proc.Start() |> ignore
+        let output = proc.StandardOutput.ReadToEnd()
+        proc.WaitForExit()
 
-    if proc.ExitCode <> 0 then
-        raise (Exception("Failed to get video duration"))
+        if proc.ExitCode <> 0 then
+            raise (Exception("Failed to get video duration"))
 
-    Double.Parse(output)
+        Double.Parse(output)
 
-let captureFrame videoFilePath time outputPath =
-    let t = sprintf "%0.3f" time
-    let parameters = $"-ss {t} -i \"{videoFilePath}\" -vframes 1 \"{outputPath}\""
+    let captureFrame videoFilePath time outputPath =
+        let t = sprintf "%0.3f" time
+        let parameters = $"-ss {t} -i \"{videoFilePath}\" -vframes 1 \"{outputPath}\""
 
-    use proc = new Process()
-    proc.StartInfo.FileName <- "ffmpeg"
-    proc.StartInfo.Arguments <- parameters
-    proc.StartInfo.RedirectStandardOutput <- true
-    proc.StartInfo.UseShellExecute <- false
-    proc.StartInfo.CreateNoWindow <- true
+        use proc = new Process()
+        proc.StartInfo.FileName <- "ffmpeg"
+        proc.StartInfo.Arguments <- parameters
+        proc.StartInfo.RedirectStandardOutput <- true
+        proc.StartInfo.UseShellExecute <- false
+        proc.StartInfo.CreateNoWindow <- true
 
-    proc.Start() |> ignore
-    proc.WaitForExit()
+        proc.Start() |> ignore
+        proc.WaitForExit()
 
-    if proc.ExitCode <> 0 then
-        raise (Exception("Failed to capture frame"))
+        if proc.ExitCode <> 0 then
+            raise (Exception("Failed to capture frame"))
 
-let getMiddleFrameBytes videoFilePath =
-    let duration = getVideoDuration videoFilePath
-    let middleTime = duration / 2.0
+    let getMiddleFrameBytes videoFilePath =
+        let duration = getVideoDuration videoFilePath
+        let middleTime = duration / 2.0
 
-    let outputPath = Path.GetTempFileName()
-    File.Delete(outputPath)
-    let outputPath = Path.ChangeExtension(outputPath, "jpg")
+        let outputPath = Path.GetTempFileName()
+        File.Delete(outputPath)
+        let outputPath = Path.ChangeExtension(outputPath, "jpg")
 
-    captureFrame videoFilePath middleTime outputPath
+        captureFrame videoFilePath middleTime outputPath
 
-    let imageBytes = File.ReadAllBytes(outputPath)
+        let imageBytes = File.ReadAllBytes(outputPath)
 
-    File.Delete(outputPath)
+        File.Delete(outputPath)
 
-    imageBytes
+        imageBytes
